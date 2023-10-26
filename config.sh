7669d1de2c3e5ede0be9238fffa0950ccbce0541
Carlos Baraza Haro <carlos.baraza@ieee.org>
2023-10-26 10:11:36 +0200

    wip

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 MODEL_URL="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf"
-MODEL_DIR="~/llm-server/models"
+MODEL_DIR=~/llm-server/models
 LLAMA_THREADS=4
 LLAMA_GPU_LAYERS=35
